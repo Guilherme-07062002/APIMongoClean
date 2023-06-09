7f0b134f026ccd3757b3d176c61c6a0df4d28fd3
Guilherme-07062002 <guilherme762002@gmail.com>
2023-06-09 11:59:56 -0300

    organização repositorio mongo

--- src/repositories/implementations/MongoDatabaseUserRepository.ts
+++ src/repositories/implementations/MongoDatabaseUserRepository.ts
@@ -1,19 +1,21 @@
 import { User } from '../../entities/User';
 import { IUserRepository } from '../IUserRepository';
-import userModel, { UserDocument } from '../../providers/MongoDatabaseProvider/models/userModel';
+import MongoUserModel from '../../providers/MongoDatabaseProvider/models/MongoUserModel';
 
 /** Repositório para comunicação com o MongoDB */
 export class MongoDatabaseUserRepository implements IUserRepository {
 
     async save(user: User): Promise<void> {
         try {
-            const newUser: UserDocument = new userModel(user);
-
-            // Salva o usuário no banco de dados
-            await newUser.save().then(() =>{
-                console.log(`O usuário ${user.name} foi cadastrado.`);
+            // Crie uma nova instância do modelo do usuário com os dados fornecidos
+            const newUser = new MongoUserModel({
+                name: user.name,
+                email: user.email,
             });
 
+            // Salva o usuário
+            await newUser.save();
+            console.log(`O usuário ${user.name} foi cadastrado.`);
         } catch (error) {
             console.error('Erro ao salvar o usuário:', error);
         }
